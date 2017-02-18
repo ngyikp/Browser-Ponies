687c5f694875c2892041bf3f2bbfa173269bde7c
Ng Yik Phang <ngyikp@gmail.com>
2017-02-18 20:50:14 +0800

    Added "level 2" Changelings by Bot-chan
    https://github.com/RoosterDragon/Desktop-Ponies/commit/2b95eb050eb6e7e2e0ea15390f638c2dd29fb9ad

--- scripts/prepare_ponies.sh
+++ scripts/prepare_ponies.sh
@@ -3,6 +3,12 @@
 rm -rf "ponies/.DS_Store"
 rm -rf "ponies/Random Pony"
 mv "ponies/Jesús Pezuña" "ponies/Jesus Pezuna"
+mv "ponies/Changeling (Lv2) #1" "ponies/Changeling (Lv2) 1"
+mv "ponies/Changeling (Lv2) #2" "ponies/Changeling (Lv2) 2"
+mv "ponies/Changeling (Lv2) #3" "ponies/Changeling (Lv2) 3"
+mv "ponies/Changeling (Lv2) #4" "ponies/Changeling (Lv2) 4"
+mv "ponies/Changeling (Lv2) #5" "ponies/Changeling (Lv2) 5"
+mv "ponies/Changeling (Lv2) #6" "ponies/Changeling (Lv2) 6"
 
 echo Repairing names
 ./scripts/repair_names.py ponies/
